b3a8c99b8510c629ab4634fa693dd7d327f0b028
abdumannopov802 <154593414+abdumannopov802@users.noreply.github.com>
2024-02-13 12:51:31 -0800

    some changes

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -3,4 +3,10 @@ from .views import *
 
 urlpatterns = [
     path('', homeView, name='home'),
+    path('portfolio/', portfolioView, name='portfolio'),
+    path('about/', aboutView, name='about'),
+    path('service/', myserviceView, name='service'),
+    path('resume/', resumeView, name='resume'),
+    path('blog/', blogView, name='blog'),
+    path('contact/', contactView, name='contact'),
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def homeView(request):
     return render(request, 'home.html', {})
+
+def resumeView(request):
+    return render(request, 'resume.html', {})
+
+def portfolioView(request):
+    return render(request, 'portfolio.html', {})
+
+def aboutView(request):
+    return render(request, 'about.html', {})
+
+def myserviceView(request):
+    return render(request, 'service.html', {})
+
+def blogView(request):
+    return render(request, 'blog.html', {})
+
+def contactView(request):
+    return render(request, 'contact.html', {})
